7dd0d395b83f0867cb46602e5c50c6c320e78fbf
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-02 16:27:13 -0700

    add merchant model

--- db/migrate/20151102232351_create_invoices.rb
+++ db/migrate/20151102232351_create_invoices.rb
@@ -1,7 +1,7 @@
 class CreateInvoices < ActiveRecord::Migration
   def change
     create_table :invoices do |t|
-      t.references :customer_id, index: true, foreign_key: true
+      t.references :customer, index: true, foreign_key: true
       t.string :status
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102230138) do
+ActiveRecord::Schema.define(version: 20151102232653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,4 +32,28 @@ ActiveRecord::Schema.define(version: 20151102230138) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "invoices", force: :cascade do |t|
+    t.integer  "customer_id"
+    t.string   "status"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "invoices", ["customer_id"], name: "index_invoices_on_customer_id", using: :btree
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "unit_price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "merchants", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "invoices", "customers"
 end
