64c69840789728d82aff8712f439b792105c3d21
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-02 16:04:46 -0700

    add invoice_item model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102225834) do
+ActiveRecord::Schema.define(version: 20151102230138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20151102225834) do
   create_table "customers", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "invoice_items", force: :cascade do |t|
+    t.integer  "item_id"
+    t.integer  "invoice_id"
+    t.integer  "quantity"
+    t.integer  "unit_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
