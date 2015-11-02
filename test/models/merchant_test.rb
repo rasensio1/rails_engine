93816d8b746d226af5c649048585ae050e9e53c8
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-02 16:51:23 -0700

    add merchant model tests

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class MerchantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "is valid" do
+     merch = Merchant.new(name: "Ryan's Cafe")
+
+     assert merch.valid?
+   end
+
+   test "is invalid without a name" do
+     merch = Merchant.new()
+
+     refute merch.valid?
+   end
 end
