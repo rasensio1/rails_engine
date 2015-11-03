a12dbaf9634fa9ad7b1d672b5804d19c3142555a
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-03 13:46:41 -0700

    add test for invoice#invoice_items

--- test/controllers/api/v1/invoices/invoice_items_controller_test.rb
+++ test/controllers/api/v1/invoices/invoice_items_controller_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class Api::V1::Invoices::InvoiceItemsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#index" do
+    inv = create_invoice_items
+
+    get :index, id: inv.id
+
+    assert_equal 2, json.count
+  end
+
+  def create_invoice_items
+    Merchant.create(name: "Yeah")
+    Merchant.create(name: "Yo")
+    Customer.create(first_name: "Hi", last_name: "YOYO")
+    Customer.create(first_name: "yo", last_name: "yo")
+    inv = Invoice.create(customer_id: 1, merchant_id: 2)
+    Invoice.create(customer_id: 2, merchant_id: 3)
+    Invoice.create(customer_id: 1, merchant_id: 1)
+    InvoiceItem.create(quantity: 1, unit_price: 2, item_id: 1, invoice_id: inv.id)
+    InvoiceItem.create(quantity: 1, unit_price: 2, item_id: 1, invoice_id: inv.id)
+    InvoiceItem.create(quantity: 1, unit_price: 2, item_id: 3, invoice_id: 2)
+    inv
+  end
 end
