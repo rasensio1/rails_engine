3ce2cb9637efc76be90d1c46d6113299c7f8db98
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-03 14:27:19 -0700

    add test for invoice_item#item

--- app/controllers/api/v1/invoice_items/item_controller.rb
+++ app/controllers/api/v1/invoice_items/item_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::InvoiceItems::ItemController < ApplicationController
   respond_to :json
 
   def show
-    respond_with Item.joins(:invoice_items).where("invoice_items.id" => params[:id]).uniq
+    respond_with Item.joins(:invoice_items).where("invoice_items.id" => params[:id]).first
   end
 end
