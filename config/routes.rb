b57bd4304a916cd17eec650f29e4c78975b79454
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-02 19:52:34 -0700

    add items controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index], defaults: {format: :json}
+      resources :items, only: [:index], defaults: {format: :json}
     end
   end
 end
