4ecbdb00d35f3221911257fabebe65cd3c351d1e
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-02 20:02:10 -0700

    add show for merchants

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index], defaults: {format: :json}
+      resources :merchants, only: [:index, :show], defaults: {format: :json}
       resources :items, only: [:index], defaults: {format: :json}
       resources :invoice_items, only: [:index], defaults: {format: :json}
       resources :invoices, only: [:index], defaults: {format: :json}
