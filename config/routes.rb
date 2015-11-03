a2c4105502e1c07af335c07b48fa7f7419d547ec
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-02 20:04:05 -0700

    add show for invoice items

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :merchants, only: [:index, :show], defaults: {format: :json}
       resources :items, only: [:index, :show], defaults: {format: :json}
-      resources :invoice_items, only: [:index], defaults: {format: :json}
+      resources :invoice_items, only: [:index, :show], defaults: {format: :json}
       resources :invoices, only: [:index], defaults: {format: :json}
       resources :transactions, only: [:index], defaults: {format: :json}
       resources :customers, only: [:index], defaults: {format: :json}
