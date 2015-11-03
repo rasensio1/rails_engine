a36a776edd86ee9d43549ec2344f4fd799e7a3b1
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-02 20:56:24 -0700

    removes default from routes. adds before action to set request type to json

--- config/routes.rb
+++ config/routes.rb
@@ -3,29 +3,34 @@ Rails.application.routes.draw do
     namespace :v1 do
       get "/:object/random", to: "random#show", defaults: {format: :json}
 
-      get "/merchants/find_all", to: "merchants#find_all", defaults: {format: :json}
-      get "/merchants/find", to: "merchants#find", defaults: {format: :json}
-      resources :merchants, only: [:index, :show], defaults: {format: :json}
+      get "/merchants/find_all", to: "merchants#find_all"
+      get "/merchants/find", to: "merchants#find"
+      resources :merchants, only: [:index, :show]
 
-      get "/items/find_all", to: "items#find_all", defaults: {format: :json}
-      get "/items/find", to: "items#find", defaults: {format: :json}
-      resources :items, only: [:index, :show], defaults: {format: :json}
+      namespace :merchants do
+        get "/:id/items", to: "items#index"
+      end
 
-      get "/invoice_items/find_all", to: "invoice_items#find_all", defaults: {format: :json}
-      get "/invoice_items/find", to: "invoice_items#find", defaults: {format: :json}
-      resources :invoice_items, only: [:index, :show], defaults: {format: :json}
+      get "/items/find_all", to: "items#find_all"
+      get "/items/find", to: "items#find"
+      resources :items, only: [:index, :show]
 
-      get "/invoices/find_all", to: "invoices#find_all", defaults: {format: :json}
-      get "/invoices/find", to: "invoices#find", defaults: {format: :json}
-      resources :invoices, only: [:index, :show], defaults: {format: :json}
+      get "/invoice_items/find_all", to: "invoice_items#find_all"
+      get "/invoice_items/find", to: "invoice_items#find"
+      resources :invoice_items, only: [:index, :show]
 
-      get "/transactions/find_all", to: "transactions#find_all", defaults: {format: :json}
-      get "/transactions/find", to: "transactions#find", defaults: {format: :json}
-      resources :transactions, only: [:index, :show], defaults: {format: :json}
+      get "/invoices/find_all", to: "invoices#find_all"
+      get "/invoices/find", to: "invoices#find"
+      resources :invoices, only: [:index, :show]
 
-      get "/customers/find_all", to: "customers#find_all", defaults: {format: :json}
-      get "/customers/find", to: "customers#find", defaults: {format: :json}
-      resources :customers, only: [:index, :show], defaults: {format: :json}
+      get "/transactions/find_all", to: "transactions#find_all"
+      get "/transactions/find", to: "transactions#find"
+      resources :transactions, only: [:index, :show]
+
+      get "/customers/find_all", to: "customers#find_all"
+      get "/customers/find", to: "customers#find"
+      resources :customers, only: [:index, :show]
     end
   end
+
 end
