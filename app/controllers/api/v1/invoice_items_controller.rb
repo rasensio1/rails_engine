fe18c9920596775919ba380f25ce8ea16c0dd9ab
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-02 19:54:26 -0700

    add route and controller for invoice items

---
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::InvoiceItemsController < ApplicationController
+  respond_to :json
+  def index
+    respond_with InvoiceItem.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :merchants, only: [:index], defaults: {format: :json}
       resources :items, only: [:index], defaults: {format: :json}
+      resources :invoice_items, only: [:index], defaults: {format: :json}
     end
   end
 end
