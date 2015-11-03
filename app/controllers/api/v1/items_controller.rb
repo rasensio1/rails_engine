54e8c84d0d656c1164802dcc17d364a15f969435
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-03 08:32:18 -0700

    Display unit_price as dollar amount. Add method to convert unit_price to dollars. removes unit_price from JSON export.

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   def show
-    respond_with Item.find(params[:id])
+    respond_with Item.find(params[:id]).to_json(methods: :price, except: :unit_price)
   end
 
   def find
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,10 @@ class Item < ActiveRecord::Base
   has_many :invoices, through: :invoice_items
 
   validates :name, :description, :unit_price, :merchant_id, presence: true
+
+  def price 
+    unit_price/100.00
+  end
+
 end
 
