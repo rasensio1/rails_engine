414ee16a074af8e33b19cd7a411e7e07c05829a5
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-03 14:41:35 -0700

    add customer#transactions

--- test/controllers/api/v1/customers/transactions_controller_test.rb
+++ test/controllers/api/v1/customers/transactions_controller_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class Api::V1::Customers::TransactionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#index" do
+    cust = create_invoices
+    get :index, id: cust.id
+
+    assert_equal 2, json.count
+  end
+
+  def create_invoices
+    m =  Merchant.create(name: "Yeah")
+    m2 =  Merchant.create(name: "OK")
+    c = Customer.create(first_name: "Hi", last_name: "YOYO")
+    c2 = Customer.create(first_name: "yo", last_name: "yo")
+    inv = Invoice.create(customer_id: c.id, merchant_id: m.id)
+    inv2 = Invoice.create(customer_id: c2.id, merchant_id: m2.id)
+    inv3 = Invoice.create(customer_id: c.id, merchant_id: m2.id)
+    Transaction.create(invoice_id: inv.id)
+    Transaction.create(invoice_id: inv.id)
+    Transaction.create(invoice_id: inv2.id)
+    c
+  end
 end
