c7f029c7e5845b5ea0b70a135f8d1d85826def63
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-03 14:13:32 -0700

    add test for invoice#merchant

--- app/controllers/api/v1/invoices/merchant_controller.rb
+++ app/controllers/api/v1/invoices/merchant_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::Invoices::MerchantController < ApplicationController
   respond_to :json
 
   def show
-    respond_with Merchant.joins(:invoices).where("invoices.merchant_id" => params[:id]).uniq
+    respond_with Merchant.joins(:invoices).where("invoices.id" => params[:id]).first
   end
 end
--- test/controllers/api/v1/invoices/merchant_controller_test.rb
+++ test/controllers/api/v1/invoices/merchant_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class Api::V1::Invoices::MerchantControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#index" do
+    inv = create_merchants
+
+    get :show, id: inv.id
+
+    assert_equal "Yeah", json["name"]
+  end
+
+  def create_merchants
+    m =  Merchant.create(name: "Yeah")
+    m2 =  Merchant.create(name: "OK")
+    c = Customer.create(first_name: "Hi", last_name: "YOYO")
+    c2 = Customer.create(first_name: "yo", last_name: "yo")
+    inv = Invoice.create(customer_id: c.id, merchant_id: m.id)
+    inv2 = Invoice.create(customer_id: c2.id, merchant_id: m2.id)
+    inv
+  end
 end
