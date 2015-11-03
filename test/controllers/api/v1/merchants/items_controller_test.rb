c21c72d366ed524482655a5617cb9858d5abc33e
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-03 13:37:44 -0700

    add test for merchant#items

--- test/controllers/api/v1/merchants/items_controller_test.rb
+++ test/controllers/api/v1/merchants/items_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class Api::V1::Merchants::ItemsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "#index" do
+    create_items
+    merch = Merchant.find_by(name: "yeah")
+    get :index, id: merch.id
+
+    assert_equal 2, json.count
+  end
+
+  def create_items
+    merch = Merchant.create(name: "yeah")
+    merch2 = Merchant.create(name: "ok")
+    Item.create(name: "Yeah", description: "also good", unit_price: 1234, merchant_id: merch.id)
+    Item.create(name: "YO", description: "best", unit_price: 1234, merchant_id: merch.id)
+    Item.create(name: "OKOK", description: "best", unit_price: 1234, merchant_id: merch2.id)
+  end
 end
