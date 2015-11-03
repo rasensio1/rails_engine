d519c00f80dc634f352627f82778a36405ae5ba8
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-02 22:15:15 -0700

    adds merchant for an item

---
+++ app/controllers/api/v1/items/merchant_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::Items::MerchantController < ApplicationController
+  respond_to :json
+
+  def show 
+    respond_with Merchant.joins(:items).where("items.id" =>  params[:id])
+  end
+end
