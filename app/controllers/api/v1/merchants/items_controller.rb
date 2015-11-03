660b931ab4858c1ef4247134936ca2148f17f262
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-02 21:11:50 -0700

    removes format helper from items_controller

--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -5,9 +5,4 @@ class Api::V1::Merchants::ItemsController < ApplicationController
   def index 
     respond_with Item.where(merchant_id: params[:id])
   end
-
-  private
-    def set_default_response_format
-      request.format = :json
-    end
 end
