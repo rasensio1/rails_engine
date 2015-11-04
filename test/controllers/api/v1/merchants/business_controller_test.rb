10404710ed1439728f43644faa8c5fc4dd113046
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-03 17:58:46 -0700

    add test for most revenue

--- test/controllers/api/v1/merchants/business_controller_test.rb
+++ test/controllers/api/v1/merchants/business_controller_test.rb
@@ -11,6 +11,18 @@ class Api::V1::Merchants::BusinessControllerTest < ActionController::TestCase
     assert_equal 800, json.first["revenue"]
   end
 
+  test "#most_revenue" do
+    merch = create_merchant_1
+    merch2 = create_merchant_2
+    merch3 = create_merchant_3
+
+    get :most_revenue, top: 2
+
+    assert_response :success
+    assert_equal 800, json.first["revenue"]
+    assert_equal 300, json.second["revenue"]
+  end
+
    def create_merchant_1
      m = Merchant.create(name: "First")
      c1 = Customer.create(first_name: "Hi", last_name: "YOYO")
@@ -29,6 +41,16 @@ class Api::V1::Merchants::BusinessControllerTest < ActionController::TestCase
      Transaction.create(invoice_id: i1.id, result: "failed")
      Transaction.create(invoice_id: i2.id, result: "success")
      Transaction.create(invoice_id: i3.id, result: "failed")
+     m
+   end
+
+   def create_merchant_2
+     m = Merchant.create(name: "First")
+     c = Customer.create(first_name: "Hi", last_name: "YOYO")
+     i = Invoice.create(customer_id: c1.id, merchant_id: m.id)
+     item = Item.create(name: "item", description: "ok", unit_price: 100, merchant_id: m.id)
+     InvoiceItem.create(quantity: 1, unit_price: 300, item_id: item.id, invoice_id: i1.id)
+     Transaction.create(invoice_id: i.id, result: "success")
      m
    end
 end
